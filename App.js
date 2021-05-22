18efac2d95e6ea26cc6b88a042e631e57cc96568
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-05-22 14:21:50 -0400

    Funcionando

--- App.js
+++ App.js
@@ -11,59 +11,66 @@ import {
 import Button from './src/componentes/Button'
 import Display from './src/componentes/Display'
 
-export default class App extends Component {
-  state = {
-    displayValue: "0",
-    valor1: 0,
-    valor2: 0,
-    operacaoIniciada: false,
-  } 
 
+const initialState = {
+ displayValue: "0",
+ clearDisplay: false,
+ operation: null,
+ values: [0,0],
+ current: 0,   
+}
+
+
+export default class App extends Component {
   
+  state = {...initialState} 
 
   addDigit = n => {
-
-    if(this.state.operacaoIniciada){
-      if(n == '.'){
-        this.setState({displayValue: '0.', operacaoIniciada: false});
-        return
+      
+      const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay
+      
+      if( n === "." && !clearDisplay && this.state.displayValue.includes(".") ){
+        return;
       }
-      this.setState({displayValue: n, operacaoIniciada: false});
+      
+      const currentValue = clearDisplay ? '' : this.state.displayValue
+      const displayValue = currentValue + n
+      this.setState({displayValue, clearDisplay: false})
+      
+      if(n !== '.'){
+        const newValue = parseFloat(displayValue)
+        const values = [...this.state.values]
+        values[this.state.current] = newValue
+        this.setState({values})
+      }
 
-      return
     }
-
-    var aux = this.state.displayValue;
-    if(this.state.displayValue.length >=15){
-      return
-    }
-    if (this.state.displayValue === '0' && n=="."){
-      this.setState({displayValue: '0.'})
-      return
-    }
-
-    if( n=== '.' && this.state.displayValue.includes('.')){
-      return
-    }
-    if(this.state.displayValue === '0'){
-      this.setState({displayValue: n})
-      return
-    }
-   
-    this.setState({displayValue: aux + n})
-  }
 
   clearDisplay = () => {
-    this.setState({displayValue: '0'})
+    this.setState({... initialState})
   }
 
   setOperation = operation =>{
-      if (operation != "="){
-        if(operation == "+"){
-       
-      }
-        this.setState({operacaoIniciada: true})
-      }
+    
+     if(this.state.current == 0){
+       this.setState({operation, current: 1, clearDisplay: true})
+     }else{
+       const equals = operation === "0"
+       const values = [... this.state.values]
+
+       try {
+         values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+       } catch (error) {
+         values[0] = this.state.values[0]
+       }
+       values[1] = this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+       })
+     }
 
   }
   
@@ -72,14 +79,13 @@ export default class App extends Component {
     <SafeAreaView style = {styles.container} >
      <Display  length = {this.state.displayValue.length} value={this.state.displayValue}/>
      <View style = {styles.buttons}>
-      <Button label="C" onClick={this.clearDisplay} />
-      <Button label="()"  />
+      <Button label="C" tamDouble onClick={this.clearDisplay} />
       <Button label="%"  onClick={this.setOperation} />
-      <Button operation label="÷" onClick={this.setOperation} />
+      <Button operation label="/" onClick={this.setOperation} />
       <Button label="7" onClick={this.addDigit} />
        <Button label="8" onClick={this.addDigit} />
        <Button label="9" onClick={this.addDigit} />
-       <Button operation label="X" onClick={this.setOperation} />
+       <Button operation label="*" onClick={this.setOperation} />
        <Button label="4" onClick={this.addDigit} />
        <Button label="5" onClick={this.addDigit}/>
        <Button label="6" onClick={this.addDigit} />
